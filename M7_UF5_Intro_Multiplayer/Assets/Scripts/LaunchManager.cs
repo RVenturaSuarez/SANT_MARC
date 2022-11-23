0cac3cdab7369c782ca64fd09256ebe859336bd1
RVenturaSuarez <rventurasuarez@gmail.com>
2022-11-23 17:04:25 +0100

    Crear random room
    Se ha creado la lógica para crear salas aleatorias y conectarse desde otra room

--- M7_UF5_Intro_Multiplayer/Assets/Scripts/LaunchManager.cs
+++ M7_UF5_Intro_Multiplayer/Assets/Scripts/LaunchManager.cs
@@ -1,22 +1,113 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using EasyUI.Toast;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using Random = UnityEngine.Random;
 
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
-    void Start()
+    [Header("---- PANELS ----"), Space(10)] 
+    [SerializeField] private GameObject panel_enter_game;
+    [SerializeField] private GameObject panel_conection_status;
+    [SerializeField] private GameObject panel_lobby;
+    [SerializeField] private List<GameObject> panels_list = new List<GameObject>();
+
+
+    [Header("---- OBJECTS PANEL ENTER GAME ----"),Space(10)]
+    [SerializeField] private TMP_InputField playerName_inputField;
+
+    
+    private void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        OcultarTodosLosPaneles();
+        panel_enter_game.SetActive(true);
     }
+
+
+    public void ConnectToPhotonServer()
+    {
+        // Verificamos que nuestro input field tenga texto
+        if (string.IsNullOrEmpty(playerName_inputField.text))
+        {
+            Toast.Show("Necesitas introducir datos");
+        }
+        else
+        {
+            // Cambiamos el nickname de PhotonNetwork por el valor de texto del input field
+            PhotonNetwork.NickName = playerName_inputField.text;
+            PhotonNetwork.ConnectUsingSettings();
+            // Mostramos al usuario el siguiente panel despues de establecer la conexión
+            OcultarTodosLosPaneles();
+            panel_conection_status.SetActive(true);
+        }
+    }
+
+    // Método que trata de conectar al usuario a una Room aleatoria, en caso de no existir llamaremos
+    // al método override OnJoinRandomFailed
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + Random.Range(0, 10000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 10;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+
+    }
+    
+
+    #region Override Photon
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Conectando a los servidores de Photon");
+        Debug.Log("Conectando a los servidores de Photon con el usuario " + PhotonNetwork.NickName);
+        OcultarTodosLosPaneles();
+        panel_lobby.SetActive(true);
     }
 
     public override void OnConnected()
     {
         Debug.Log("Conectando a Internet");
     }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName +  " se ha conectado a " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " se ha conectado a " + PhotonNetwork.CurrentRoom.Name);
+        Debug.Log("Numero de players: " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    #endregion
+
+    
+
+    private void OcultarTodosLosPaneles()
+    {
+        foreach (GameObject panel in panels_list)
+        {
+            panel.SetActive(false);
+        }
+    }
+    
 }
